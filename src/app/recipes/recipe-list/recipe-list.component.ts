17f06920ad9fc37d6ff2a4c989f6f123b12a8781
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2020-01-12 23:39:22 +0000

    Adding recipes to the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Summer Colors',
+      'Delicious and rich on vitamins',
+      'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+    ),
+    new Recipe(
+      'Nuts and Yogurt',
+      'Simple and easy to prepare',
+      'https://images.pexels.com/photos/1646711/pexels-photo-1646711.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+    ),
+  ];
 
 }
