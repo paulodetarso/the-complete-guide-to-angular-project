f083d9459b31ce74fa9b8ff651a7a93819ba1ed4
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2020-01-13 00:15:03 +0000

    Moving the recipe details to the `RecipeItemComponent`

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent {
+
+  @Input()
+  recipe: Recipe;
 
 }
