b6fe19392308333337b656004f47cb306a86fd4c
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2020-01-13 17:28:21 +0000

    Adding the directive to control dropdown menus

---
+++ src/app/shared/directive/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  constructor(
+    private elRef: ElementRef,
+  ) { }
+
+  @HostBinding('class.open')
+  isOpen = false;
+
+  @HostListener('document:click', ['$event'])
+  toggleOpen(event: Event): void {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+}
